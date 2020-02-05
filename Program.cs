78fc74b654ed13ae1e510011c925c81169619ed4
Aolayyan8570 <60192008+Aolayyan8570@users.noreply.github.com>
2020-02-05 10:46:44 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppTestGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
